cc8c5b12589aac951f2c890540ec92dd262c7374
tamathecxder <yudistiraeka.pratama012@gmail.com>
2023-08-18 14:17:54 +0700

    chore: completing logout controller

---
+++ app/Http/Controllers/Api/LogoutController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use PHPOpenSourceSaver\JWTAuth\Facades\JWTAuth;
+
+class LogoutController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request)
+    {
+        $removeToken = JWTAuth::invalidate(JWTAuth::getToken());
+
+        if ($removeToken) {
+            return response()->json([
+                'success' => true,
+                'message' => 'Logout Berhasil'
+            ], 200);
+        }
+    }
+}
