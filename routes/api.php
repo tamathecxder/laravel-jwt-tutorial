31c4c91ac5b1e81f0377860a70a00e60faf88aef
tamathecxder <yudistiraeka.pratama012@gmail.com>
2023-08-18 14:18:30 +0700

    chore: update api routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Api\LoginController;
+use App\Http\Controllers\Api\LogoutController;
 use App\Http\Controllers\Api\RegisterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -15,4 +17,28 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+/**
+ * route "/register"
+ * @method POST
+ */
 Route::post('/register', RegisterController::class)->name('register');
+
+/**
+ * route "/login"
+ * @method POST
+ */
+Route::post('/login', LoginController::class)->name('login');
+
+/**
+ * route "/user"
+ * @method GET
+ */
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+/**
+ * route "/logout"
+ * @method POST
+ */
+Route::post('/logout', LogoutController::class)->name('logout');
